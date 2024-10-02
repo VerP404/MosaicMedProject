ad1d115a473ce0e1f0fea41d44b1de88f9f11310
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-10-02 09:44:28 +0300

    fix: исправлены названия приложений в админке

--- apps/personnel/apps.py
+++ apps/personnel/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PersonnelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.personnel'
+    verbose_name = 'Персонал'
