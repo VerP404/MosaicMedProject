b424911cc66ad30117604989b9afa5d6791e1688
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-10-10 16:21:13 +0300

    fix: исправлен manage.py

--- manage.py
+++ manage.py
@@ -2,25 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import subprocess
-
-from threading import Thread
-from config.settings import BASE_DIR
-
-
-def run_dash_app():
-    """Запуск Dash приложения."""
-    dash_app_path = os.path.join(BASE_DIR, 'apps', 'analytical_app', 'index.py')
-    dash_command = [sys.executable, dash_app_path]
-    dash_process = subprocess.Popen(dash_command)
-    try:
-        dash_process.wait()
-    except KeyboardInterrupt:
-        dash_process.terminate()
 
 
 def main():
-    """Запуск Django приложения."""
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -34,13 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # Создание потока для запуска Dash приложения
-    dash_thread = Thread(target=run_dash_app)
-    dash_thread.start()
-
-    # Запуск Django приложения
-    try:
-        main()
-    except KeyboardInterrupt:
-        # Завершение процессов при прерывании
-        dash_thread.join()
+    main()
