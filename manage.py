789a40f24c7515d639ab1159eecb240e4d2f1853
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-10-10 14:30:45 +0300

    fix: единый запуск приложений через исправление manage.py и subprocess

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import subprocess
+
+from config.settings import BASE_DIR
 
 
 def main():
@@ -19,4 +22,16 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # Путь к файлу Dash приложения (index.py)
+    dash_app_path = os.path.join(BASE_DIR, 'apps', 'analytical_app', 'index.py')
+
+    # Запуск Dash приложения
+    dash_command = [sys.executable, dash_app_path]
+    dash_process = subprocess.Popen(dash_command)
+
+    try:
+        # Запуск Django приложения
+        main()
+    except KeyboardInterrupt:
+        # Завершение процессов при прерывании
+        dash_process.terminate()
