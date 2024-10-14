5910e4d893d05b8257823be0d88154deb00291b0
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-10-14 15:55:24 +0300

    fix: исправлены настройки подключения

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,11 +5,11 @@ LOGGING['root']['level'] = 'DEBUG'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('Name'),
-        'USER': env('USER'),
-        'PASSWORD': env('PASSWORD'),
-        'HOST': env('HOST'),
-        'PORT': env('PORT'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
