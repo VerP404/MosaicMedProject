eca52dd7270b2208e5a27f87e4e31731f0a4669c
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-10-16 16:40:01 +0300

    fix: Добавлено кеширование статики

--- apps/analytical_app/app.py
+++ apps/analytical_app/app.py
@@ -12,9 +12,11 @@ ASSETS_PATH = os.path.join(CURRENT_DIRECTORY, 'assets')
 app = dash.Dash(__name__,
                 suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP],
-                assets_folder=ASSETS_PATH
+                assets_folder=ASSETS_PATH,
+                serve_locally=True
                 )
 
+app.server.config['SEND_FILE_MAX_AGE_DEFAULT'] = 31536000
 app.title = 'МозаикаМед-Аналитика'
 
 server = app.server
