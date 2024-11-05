d089254d3c2314e964216fd36ece2b6ae4fd341f
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-11-06 02:01:55 +0300

    feat: доппараметры для мозилы

--- apps/data_loader/selenium/oms.py
+++ apps/data_loader/selenium/oms.py
@@ -43,6 +43,10 @@ def selenium_oms(username, password, start_date, end_date, start_date_treatment)
     try:
         # Настройка опций для Firefox
         options = Options()
+        options.headless = True
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         options.add_argument("--headless")  # Используем headless режим для работы на сервере
 
         # Автоматическая установка geckodriver с помощью webdriver-manager
