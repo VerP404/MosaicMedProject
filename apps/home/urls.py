100d6298eab5efb64540e7a982ee515df9c9cbcc
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-11-13 11:43:22 +0300

    fix: home - исправлены апи для статусов серверов

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,7 +1,10 @@
 from .views import *
 from django.urls import path
 
+from ..data_loader.views import get_table_row_counts
+
 urlpatterns = [
     path('', home, name='home'),
-    path('status/', check_system_status, name='check_system_status'),  # api для опроса сервера на работоспособность
+    path('status-server/', check_system_status, name='check_system_status'),
+    path('status-table/', get_table_row_counts, name='get_table_row_counts'),
 ]
