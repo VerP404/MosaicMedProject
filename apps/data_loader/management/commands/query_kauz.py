702920be9afdd74baebe822b2e6cd5e45ac821dc
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-12-10 11:57:21 +0300

    fix: загрузка мискауз - нулевое поле в дате обновления. стационар

--- apps/data_loader/management/commands/query_kauz.py
+++ apps/data_loader/management/commands/query_kauz.py
@@ -375,6 +375,6 @@ FROM CASESSTAC AS ca
                     WHERE a.DATACCOUNT = (SELECT MAX(sub_a.DATACCOUNT)
                                           FROM ACCOUNTS sub_a
                                           WHERE sub_a.IDCASEAMB = a.IDCASEAMB)) ACCOUNTS ON ca.ID = ACCOUNTS.IDCASEAMB
-WHERE ca.DATEUPDATE BETWEEN '{date_start}' AND '{date_end}'
+WHERE COALESCE(ca.DATEUPDATE, ca.DATEINSERT) BETWEEN '{date_start}' AND '{date_end}'
 and ca.DATENDVST like '%{year_end}%';
 """
