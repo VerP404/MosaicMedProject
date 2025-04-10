657c1011b19c84515bef8be41d7c59569f933669
Dmitriy Rodionov <dmitr.rod@gmail.com>
2025-04-10 15:41:58 +0300

    fix: дагстер - локальная автоматизация

--- mosaic_conductor/update/assets.py
+++ mosaic_conductor/update/assets.py
@@ -1,10 +1,9 @@
 import dagster as dg
 
 from mosaic_conductor.update.journal.update_status import update_status_job, daily_status_update_schedule
-from mosaic_conductor.update.servers.update import update_servers_job, servers_update_schedule
 
-all_jobs = [update_status_job, update_servers_job]
-all_schedules = [daily_status_update_schedule, servers_update_schedule]
+all_jobs = [update_status_job]
+all_schedules = [daily_status_update_schedule]
 
 defs = dg.Definitions(
     jobs=all_jobs,
