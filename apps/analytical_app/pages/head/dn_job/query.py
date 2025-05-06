4636194f0842d2cfb03f004ee5c399c64b75b2c7
Dmitriy Rodionov <dmitr.rod@gmail.com>
2025-05-06 10:34:22 +0300

    fix: аналитическая система - диспансерное наблюдение работающих. Исправлен дополнительный диагноз

--- apps/analytical_app/pages/head/dn_job/query.py
+++ apps/analytical_app/pages/head/dn_job/query.py
@@ -1,101 +1,3 @@
-def sql_head_dn_job():
-    return f"""
-    WITH combined_diagnoses AS (
-    SELECT DISTINCT
-        enp,
-        status,
-        LOWER(LEFT(main_diagnosis, POSITION(' ' IN main_diagnosis)-1)) AS ds
-    FROM data_loader_omsdata
-    WHERE main_diagnosis IS NOT NULL
-    and goal = '3'
-    UNION ALL
-
-    SELECT DISTINCT
-        enp,
-        status,
-        LOWER(LEFT(additional_diagnosis, POSITION(' ' IN additional_diagnosis)-1)) AS ds
-    FROM data_loader_omsdata
-    WHERE additional_diagnosis IS NOT NULL
-    and goal = '3'
-),
-
-job_data AS (
-    SELECT
-        job.id_iszl,
-        job.organization,
-        job.fio,
-        job.birth_date,
-        job.enp,
-        job.ds,
-        CASE
-            WHEN people.enp IS NOT NULL THEN 'да'
-            ELSE 'нет'
-        END AS attachment,
-        CASE
-            WHEN COUNT(CASE WHEN diag.enp IS NOT NULL THEN 1 END) > 0 THEN 'да'
-            ELSE 'нет'
-        END AS oms,
-        CASE
-            WHEN COUNT(CASE WHEN diag.enp IS NOT NULL AND diag.status = '3' THEN 1 END) > 0 THEN 'да'
-            ELSE 'нет'
-        END AS oms_paid
-    FROM data_loader_iszldisnabjob AS job
-    LEFT JOIN data_loader_iszlpeople AS people
-        ON job.enp = people.enp
-    LEFT JOIN combined_diagnoses AS diag
-        ON job.enp = diag.enp
-        AND LOWER(job.ds) = diag.ds
-    GROUP BY
-        job.id_iszl,
-        job.organization,
-        job.fio,
-        job.birth_date,
-        job.enp,
-        job.ds,
-        people.enp
-)
-
-SELECT
-    COUNT(*) AS "Всего записей в ИСЗЛ",                                             
-    COUNT(CASE WHEN attachment = 'да' THEN 1 END) AS "Прикреплено (заменить)",          
-    COUNT(CASE WHEN attachment = 'нет' THEN 1 END) AS "Не прикреплено",     
-    COUNT(CASE WHEN attachment = 'нет' AND oms = 'да' THEN 1 END) AS "Подано",  
-    COUNT(CASE WHEN attachment = 'нет' AND oms_paid = 'да' THEN 1 END) AS "Оплачено"  
-FROM job_data;
-    """
-
-
-def sql_head_dn_job2():
-    return f"""
-WITH parsed AS (SELECT external_id,
-                       mo_prikreplenia,
-                       org_prof_m,
-                       enp,
-                       ds,
-                       date,
-                       to_timestamp(date, 'DD.MM.YYYY HH24:MI:SS') AS ts
-                FROM load_data_dn_work_iszl)
-SELECT external_id,
-       mo_prikreplenia,
-       org_prof_m,
-       enp,
-       UPPER(ds)              AS ds,
-       date,
-       EXTRACT(MONTH FROM ts) AS month,
-       EXTRACT(YEAR FROM ts)  AS year,
-       CASE
-           WHEN ROW_NUMBER() OVER (
-               PARTITION BY enp, UPPER(ds), EXTRACT(YEAR FROM ts)
-               ORDER BY external_id
-               ) = 1
-               THEN FALSE
-           ELSE TRUE
-           END                AS duplicate
-FROM parsed
-ORDER BY enp, ds, year, external_id
-    """
-
-
 def sql_head_dn_job_nested(year: int) -> str:
     return f"""
 WITH
@@ -122,7 +24,7 @@ WITH
     WHERE EXTRACT(YEAR FROM TO_TIMESTAMP(x.date, 'DD.MM.YYYY HH24:MI:SS')) = {year}
   ),
 
-  tal AS (
+   tal AS (
     SELECT
       t.enp,
       t.patient,
@@ -140,16 +42,19 @@ WITH
            ELSE SPLIT_PART(t.main_diagnosis, ' ', 1)
       END AS ds1,
       ARRAY(
-        SELECT word
-        FROM unnest(string_to_array(t.additional_diagnosis, ' ')) AS word
-        WHERE word ~ '^[A-ZА-Я]\d{1, 2}(\.\d{1, 2})?$'
-      ) AS ds2
+            SELECT word
+            FROM unnest(string_to_array(t.additional_diagnosis, ' ')) AS word
+            WHERE word ~ '^[A-ZА-Я][0-9]{{1,2}}(\\.[0-9]{{1,2}})?$'
+          ) AS ds2
     FROM load_data_talons AS t
     WHERE t.goal = '3'
       AND EXTRACT(YEAR FROM TO_DATE(t.treatment_end, 'DD-MM-YYYY')) = {year}
-      AND (t.place_service <> '-' OR EXISTS (
-             SELECT 1 FROM load_data_dn_work_iszl d_full WHERE d_full.enp = t.enp
-          ))
+      AND (
+        t.place_service <> '-'
+        OR EXISTS (
+          SELECT 1 FROM load_data_dn_work_iszl d_full WHERE d_full.enp = t.enp
+        )
+      )
   )
 
 SELECT
